68e3a52ff1dc9a08def132a9ffeb71c3a3c2fa1d
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-10-02 22:17:49 +0530

    C/

---
+++ C/row_and_col.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int row,col;
+	int arr1[2][2];
+	int arr2[2][2];
+	int arr3[2][2];
+	for(row=0;row<2;row++)
+	{
+		for(col=0;col<2;col++)
+		{
+			arr3[r][2]=arr1[2][2]+arr2[2][2];
+		}	
+	}
+	for(row=)
+}
