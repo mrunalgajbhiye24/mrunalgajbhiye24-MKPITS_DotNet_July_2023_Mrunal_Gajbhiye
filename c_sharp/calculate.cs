1667c280d28dc5fae3f82667455dfef4e4aba8dc
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-10-12 14:18:14 +0530

    c_sharp/

---
+++ c_sharp/calculate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2, add , sub , mul,div, mod;
+            Console.WriteLine("Enter First number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            add = num1 + num2;
+            sub = num1 - num2;
+            mul = num1 * num2;
+            div = num1 / num2;
+            mod = num1 % num2;
+            Console.WriteLine("Addition :" + add);
+            Console.WriteLine("Subtraction :" + sub);
+            Console.WriteLine("Multiply :" + mul);
+            Console.WriteLine("Division :" + div);
+            Console.WriteLine("Modulus :" + mod);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ c_sharp/three_num.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace three_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z,result, res;
+            Console.WriteLine("Enter number 1");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 3");
+            z = Convert.ToInt32(Console.ReadLine());
+            result = (x + y) * z;
+            res = (x * y) + (y * z);
+            Console.WriteLine(" First total :"+result);
+            Console.WriteLine("Second total :"+res);
+            Console.ReadKey();
+
+
+        }
+    }
+}
