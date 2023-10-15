f6b2869b90d8c103602e9b9a57207f37e1c7b51e
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-10-15 22:52:52 +0530

    c_sharp/

---
+++ c_sharp/Equilateral_isosceles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Equilateral_iso_scalen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter the length of three sides of triangle ");
+            Console.Write("Enter side1: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter side2: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter side3: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a == b && b == c)
+                Console.WriteLine("Equilateral Triangle");
+            else if (a == b || b == c || a == c)
+                Console.WriteLine("Isosceles Triangle");
+            else
+                Console.WriteLine("Scalene");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ c_sharp/Triangle_Equal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Triangle_Equal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1, a2, a3;
+            Console.WriteLine("Enter the length of three sides of triangle ");
+            Console.Write("Enter angle1: ");
+            a1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter angle2: ");
+            a2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter angle3: ");
+            a3 = Convert.ToInt32(Console.ReadLine());
+
+            if (a1 + a2 + a3 == 180)
+                Console.WriteLine("Triangle is valid");
+            else
+                Console.WriteLine("Triangle is not valid");
+            Console.ReadKey();
+        }
+    }
+}
