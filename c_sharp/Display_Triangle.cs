326bf76748111ed809226d6f05db3772ddfa6f7f
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-10-16 17:26:51 +0530

    c_sharp/

---
+++ c_sharp/Display_Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, width;
+
+            Console.Write("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the desired width: ");
+            width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("         ");
+
+            for (int i = width; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(number);
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
