967191ec96148250bcdb5e36eaecc8098b1a4e7c
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-10-24 12:29:00 +0530

    c_sharp/

---
+++ c_sharp/Deposite_Withdrawl_Window.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Deposite_Withdrawl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int acntno, bal = 1000, amt, deposite, withdrwal;
+            string name;
+
+
+            acntno = Convert.ToInt32(textBox1.Text);
+            name = (textBox2.Text);
+            amt = Convert.ToInt32(textBox3.Text);
+
+
+            if (amt==0)
+            {
+
+                deposite = amt + bal;
+                label4.Text = "Account no. : " + acntno;
+                label5.Text = "Account Holder's Name: " + name;
+                label6.Text = "Iniatial Balance: " + bal;
+                label7.Text = "Total Balance: " + deposite;
+            }
+
+            else
+            {
+                withdrwal = bal - amt;
+                label4.Text = "Account no. : " + acntno;
+                label5.Text = "Account Holder's Name: " + name;
+                label6.Text = "Iniatial Balance: " + bal;
+                label7.Text = "Total Balance: " + withdrwal;
+            }
+        }
+    }
+}
---
+++ c_sharp/Grade_swicth_Window.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grade_Switch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char grade;
+            string res = null;
+            grade = Convert.ToChar(textBox1.Text);
+
+            switch (grade)
+            {
+                case 'E':
+                    res="Excellent";
+                    break;
+                case 'V':
+                    res="Very Good";
+                    break;
+                case 'G':
+                    res = "Good";
+                    break;
+                case 'A':
+                    res = "Average";
+                    break;
+                case 'F':
+                    res = "Fail";
+                    break;
+                default:
+                    res = "Invalid";
+                    break;
+
+            }
+            label2.Text = "Result: " + res;
+        }
+    }
+}
