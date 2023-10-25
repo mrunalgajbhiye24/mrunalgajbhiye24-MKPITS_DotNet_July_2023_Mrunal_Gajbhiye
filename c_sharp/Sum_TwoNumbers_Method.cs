ab17f27027b34de2345a90a41ecf1c8d89fdd6b2
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-10-25 18:32:27 +0530

    c_sharp/

---
+++ c_sharp/Sum_TwoNumbers_Method.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_TwoNumbers_Method
+{
+    internal class Program
+    {
+        static int calculate(int num1,int num2)
+        {
+            int res = 0;
+            res = num1 + num2;
+            return res;
+        }
+       
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            Console.Write("Enter 1st No: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2nd No: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            int result = calculate(number1, number2);
+            Console.WriteLine("Addition of " + number1 + " + " + number2 + " " + "is: " + result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c_sharp/ass_q2_Name_Parameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass_q2_Name_WithParameter
+{
+    internal class Program
+    {
+        static void Person(string name)
+        {
+            Console.WriteLine("Welcome friend " + name + "\n" + "Have a Nice Day");
+        }
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Enter Name: ");
+            name = Convert.ToString(Console.ReadLine());
+            Person(name);
+            Console.ReadKey();
+        }
+    }
+}
