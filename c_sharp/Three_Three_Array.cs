25906da50130c557db3ac906ce24cf54b9f3404d
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-10-29 22:02:49 +0530

    c_sharp/

---
+++ c_sharp/Three_Three_Array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _3_3_Matrix_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int[,] arr = new int[3, 3];
+            for(i=0;i<3;i++)
+            {
+               for(j=0;j<3;j++)
+               {
+                    Console.Write("Enter Element [{0},{1}] :",i, j);
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+               }
+            }
+            
+            Console.WriteLine("The Matrix is: ");
+            for (i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write(arr[i, j]+" ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
