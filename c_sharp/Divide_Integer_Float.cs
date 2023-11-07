37245103e7c62d34e3044fe8e44d493402cd6e47
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-11-07 17:21:30 +0530

    c_sharp/

---
+++ c_sharp/Divide_Integer_Float.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide_Integer_Float
+{
+    class Calculate
+    {
+        public void divide(int n1, int n2)
+        {
+            int res = n1 / n2;
+            Console.WriteLine("Multiplication of 2 Integer: " + res);
+        }
+        public void divide(float n1, float n2)
+        {
+            float res = n1 / n2;
+            Console.WriteLine("Multiplication of 2 Integer: " + res);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate cal = new Calculate();
+            cal.divide(7 ,2);
+            cal.divide(5.5f, 2.2f);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c_sharp/Multiplication_Integer_Float.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Integer_Float
+{
+    class Calculate
+    {
+        public void  Multiply(int n1, int n2)
+        {
+            int res = n1 * n2;
+            Console.WriteLine("Multiplication of 2 Integer: " + res);
+        }
+        public void Multiply(float n1, float n2)
+        {
+            float res = n1 * n2;
+            Console.WriteLine("Multiplication of 2 Integer: " + res);
+        }
+    }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            Calculate mul = new Calculate();
+            mul.Multiply(4, 5);
+            mul.Multiply(5.5f, 6.6f);
+            Console.ReadKey();
+            }
+        }
+}
