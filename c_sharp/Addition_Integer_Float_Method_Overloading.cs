1b14be84cbfb6ac35c1686e29b1212caaa2a519b
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-11-07 17:15:03 +0530

    c_sharp/

---
+++ c_sharp/Addition_Integer_Float_Method_Overloading.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition_Interger_Float
+{
+    class Calculate
+    {
+        public void addition(int n1,int n2)
+        {
+            int res = n1 + n2;
+            Console.WriteLine("Addition Of 2 Integer: " + res);
+        }
+        public void addition(float n1,float n2)
+        {
+            float res = n1 + n2;
+            Console.WriteLine("Addition of 2 Float Numbers: " + res);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate cal = new Calculate();
+            Console.WriteLine("Calling Addition of Integer Numbers");
+            cal.addition(5, 9);
+
+            Console.WriteLine();
+            Console.WriteLine("Now calling Addition of Float Numbers");
+            cal.addition(3.3f, 2.2f);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c_sharp/Subtraction_Integer_float_Method_Overloading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subtraction_Integer_Float
+{
+    class Calculate
+    {
+        public void subtraction(int n1,int n2)
+        {
+            int res = n1 - n2;
+            Console.WriteLine("Subtraction of 2 Integer Parameters: " + res);
+        }
+        public void subtraction(float n1, float n2)
+        {
+            float res = n1 - n2;
+            Console.WriteLine("Subtraction of 2 Float Parameters: " + res);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate cal = new Calculate();
+            cal.subtraction(4, 2);
+            cal.subtraction(3.3f, 2.2f);
+            Console.ReadKey();
+        }
+    }
+}
