c287b0abf764e6ad0d554480130c8245da9f8e68
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-11-16 12:41:40 +0530

    c_sharp/INHERITANCE/

---
+++ c_sharp/INHERITANCE/Single_Level_Inheritance/Inheritance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        public string name;
+    }
+    class Student : Person
+    {
+        public int rno;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.rno = 123;
+            s.name = "Purvi";
+            Console.WriteLine("Student Roll No: " + s.rno);
+            Console.WriteLine("Student Name: " + s.name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c_sharp/INHERITANCE/Single_Level_Inheritance/Single_Level_Inheritance_Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Level_Inheritance_Employee
+{
+    class Employee
+    {
+        public int salary;
+    }
+    class Programmer:Employee
+    {
+        public int bonus;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer p = new Programmer();
+            p.salary = 60000;
+            p.bonus = 2000;
+            Console.WriteLine("Employee Salary: " + p.salary);
+            Console.WriteLine("Employee Bonus: " + p.bonus);
+            Console.ReadKey();
+        }
+    }
+}
