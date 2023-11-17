2e93091e4d6263badd94bd5186005330d061b78b
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-11-17 14:50:31 +0530

    c_sharp/INHERITANCE/

---
+++ c_sharp/INHERITANCE/Hierarchical_Inheritance/BankAccount_Hierarchical_Example.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarchical_Inheritance_BankAccount_Example
+{
+    class Account
+    {
+        public int actno;
+        public int bal = 1000;
+
+        public void deposite(int amt)
+        {
+            Console.WriteLine("This is a Deposited Method of Account Class");
+        }
+    }
+    class Saving:Account
+    {
+        public void deposite(int amt)
+        {
+            int interest = 500;
+            bal = bal + amt + interest;
+            Console.WriteLine("Amount Deposited With Interest balance is:- " + bal);
+        }
+    }
+    class Current : Account
+    {
+        public void deposite(int amt)
+        {
+            bal = bal + amt;
+            Console.WriteLine("Amount Deposited Without Interest balance is:- " + bal);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saving s = new Saving();
+            s.actno = 123;
+            s.deposite(1000);
+
+            Current c = new Current();
+            c.actno = 234;
+            c.deposite(500);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c_sharp/INHERITANCE/Hierarchical_Inheritance/Hierarchical_Inheritance_Example_Two.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Hierarchical_Inheritance_Example_Two
+{
+    class Person
+    {
+        string name;
+        string address;
+        
+        public void getpersondata(string name,string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        public void displaypersondata()
+        {
+            Console.WriteLine("Name:- " + name);
+            Console.WriteLine("Address:- " + address);
+        }
+    }
+    class Student:Person
+    {
+        int rno;
+        int marks;
+
+        public void getstudentdata(int rno,int marks)
+        {
+            this.rno = rno;
+            this.marks = marks;
+        }
+        public void displaystudentdata()
+        {
+            Console.WriteLine("RollNo:- " + rno);
+            Console.WriteLine("Marks:- " + marks);
+        }
+    }
+    class Employee : Person
+    {
+        int salary;
+        string designation;
+
+        public void getemployeedata(int salary, string designation)
+        {
+            this.salary = salary;
+            this.designation = designation;
+        }
+        public void displayemployeedata()
+        {
+            Console.WriteLine("Salary:- " + salary);
+            Console.WriteLine("Designation:- " + designation);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------[Student Details]-------------");
+            Student s = new Student();
+            s.getpersondata("Rohini", "Nagpur");
+            s.getstudentdata(123, 78);
+            s.displaypersondata();
+            s.displaystudentdata();
+
+            Console.WriteLine();
+            Console.WriteLine("------------[Employee Details]-------------");
+            Employee e = new Employee();
+            e.getpersondata("Mrunal", "Pune");
+            e.getemployeedata(90000, "Manager");
+            e.displaypersondata();
+            e.displayemployeedata();
+            Console.ReadKey();
+        }
+    }
+}
