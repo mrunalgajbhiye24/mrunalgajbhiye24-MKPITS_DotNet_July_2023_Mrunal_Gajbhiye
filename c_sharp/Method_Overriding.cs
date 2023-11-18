4d1469717d1a69db9a5aabbc32f29506b1e8b956
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-11-18 10:13:00 +0530

    c_sharp/

---
+++ c_sharp/Method_Overriding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_OverRiding
+{
+    class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating....");
+        }
+    }
+    class Dog:Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating Bread....");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal d = new Dog();
+            d.eat();
+            Console.ReadKey();
+        }
+    }
+}
