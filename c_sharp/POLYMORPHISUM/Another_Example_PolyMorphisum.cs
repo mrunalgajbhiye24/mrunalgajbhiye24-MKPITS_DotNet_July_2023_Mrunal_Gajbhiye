18a9c2224089f4c088d30a562e063035fa8d0106
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-11-18 10:11:48 +0530

    c_sharp/POLYMORPHISUM/

---
+++ c_sharp/POLYMORPHISUM/Another_Example_PolyMorphisum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Another_Example_PolyMorphisum
+{
+    class Shape
+    {
+        public virtual void draw()
+        {
+            Console.WriteLine("Drawing...");
+        }
+    }
+    class Rectangle:Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing Rectangle...");
+        }
+    }
+    class Circle:Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing Circle...");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s;
+            s = new Shape();
+            s.draw();
+
+            s = new Rectangle();
+            s.draw();
+
+            s = new Circle();
+            s.draw();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c_sharp/POLYMORPHISUM/RunTime_Polymorphisum_Example_One.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runtime_PolyMorphisum_Example_One
+{
+    class Baseclass
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Hello From Base Class Show Method");
+        }
+    }
+    class Childclass:Baseclass
+    {
+        public override void show()
+        {
+            Console.WriteLine("Hello From Child Class Show Mthod");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Baseclass b = new Childclass();
+            b.show();
+            Console.ReadLine();
+        }
+    }
+}
