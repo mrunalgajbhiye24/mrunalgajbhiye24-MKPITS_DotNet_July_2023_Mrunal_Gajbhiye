4fd8bb7e431b9a0f8fdad0ed5898643303a4de5c
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-11-20 17:27:09 +0530

    c_sharp/ABSTRACTION/

---
+++ c_sharp/ABSTRACTION/Assignment_Class_Object_Example.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Class_Obejct_Example
+{
+    class Person
+    {
+        public string name;
+        public string address;
+        public int age;
+
+        public void getdata(string name,string address,int age)
+        {
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("Person Name:- " + name);
+            Console.WriteLine("Person Address:- " + address);
+            Console.WriteLine("Person Age:- " + age);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.getdata("Mrunal", "Pl.No 115 Chandra Nagar,Bhandara Road,Nagpur", 22);
+            p.displaydata();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c_sharp/ABSTRACTION/Assignment_Inheritance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Inheritance
+{
+    class Vehicle
+    {
+        public int make;
+        public string model;
+    }
+    class Car:Vehicle
+    {
+        public int numdoors;
+        public string fueltype;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            c.make = 1945;
+            c.model = "Audi";
+            c.numdoors = 4;
+            c.fueltype = "Deisel";
+            Console.WriteLine("Car Make:- " + c.make);
+            Console.WriteLine("Car Model:- "+c.model);
+            Console.WriteLine("Num of Doors in Car:- " + c.numdoors);
+            Console.WriteLine("Fuel Type in Car:- " + c.fueltype);
+            Console.ReadKey();
+        }
+    }
+}
