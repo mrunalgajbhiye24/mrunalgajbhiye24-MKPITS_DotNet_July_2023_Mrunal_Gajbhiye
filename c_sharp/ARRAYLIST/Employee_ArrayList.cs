7dc9a151da4727a9d288b2e7c70925c974152143
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-11-24 13:09:10 +0530

    c_sharp/

---
+++ c_sharp/ARRAYLIST/Employee_ArrayList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_ArrayList
+{
+    internal class Program
+    {
+        class Employee
+        {
+            public int empno;
+            public string empname;
+            public int salary;
+            public string designation;
+
+            public Employee(int empno,string empname,int salary,string designation)
+            {
+                this.empno = empno;
+                this.empname = empname;
+                this.salary = salary;
+                this.designation = designation;
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            Employee e1 = new Employee(11, "Mrunal", 50000, "Manager");
+            Employee e2 = new Employee(22, "Reeta", 30000, "Clerk");
+            Employee e3 = new Employee(33, "Manisha", 15000, "Peon");
+
+            ar.Add(e1);
+            ar.Add(e2);
+            ar.Add(e3);
+
+            foreach(Employee e in ar)
+            {
+                Console.WriteLine("Employee Id:- " + e.empno);
+                Console.WriteLine("Employee Name:- " + e.empname);
+                Console.WriteLine("Employee Salary:- " + e.salary);
+                Console.WriteLine("Employee Designation:- " + e.designation);
+                Console.WriteLine("-------------------------------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c_sharp/ARRAYLIST/Employee_WindowsForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Employee_ArrayList_Windows_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Employee emp;
+        ArrayList ar = new ArrayList();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            emp = new Employee (Convert.ToInt32(textBox1.Text),textBox2.Text, Convert.ToInt32(textBox3.Text),textBox4.Text);
+            ar.Add(emp);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(Employee emp in ar)
+            {
+                sb.Append("Employee No:- " + emp.empno+"\n");
+                sb.Append("Employee Name:- " + emp.empname+"\n");
+                sb.Append("Employee Salary:- " + emp.salary+"\n");
+                sb.Append("Employee Designation:- " + emp.designation+"\n");
+                sb.Append("------------------------------------"+"\n");
+            }
+            label5.Text = sb.ToString();
+        }
+    }
+}
