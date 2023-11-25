f5455eafac660b8c2cd76823d26203ee8e049784
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-11-25 14:25:56 +0530

    c_sharp/

---
+++ c_sharp/COLLECTIONS/Stack String Collection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace Stack_string_Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push("Amar");
+            s.Push("Ravi");
+            s.Push("Antoni");
+            foreach(string val in s)
+            {
+                Console.WriteLine("Name:- " + val);
+            }
+            Console.WriteLine("--------------------");
+            s.Push("Ankita");
+            Console.WriteLine("Last Item Added" +" "+ s.Peek());
+            Console.WriteLine("After Peek method");
+            foreach(string val in s)
+            {
+                Console.WriteLine("Name:- " + val);
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Last Item Added" + " " + s.Pop());
+            Console.WriteLine("After Pop method");
+            foreach (string val in s)
+            {
+                Console.WriteLine("Name:- " + val);
+            }
+            Console.ReadKey();
+        }
+    }
+}
