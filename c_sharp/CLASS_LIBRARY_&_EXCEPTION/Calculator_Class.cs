dc85946dfc0c30ab506679e0385d9001c589ae3a
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-12-01 15:04:01 +0530

    c_sharp/

---
+++ c_sharp/CLASS_LIBRARY_&_EXCEPTION/Calculator_Class.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Class
+{
+    public class Calculator
+    {
+        public double result { get; set; }
+
+        public void Add(double num)
+        {
+            result += num;
+        }
+        public void sub(double num)
+        {
+            result -= num;
+        }
+        public void mul(double num)
+        {
+            result *= num;
+        }
+        public void div(double num)
+        {
+            if(num!=0)
+            {
+                result /= num;
+            }
+            else
+            {
+                Console.WriteLine("Error: Cannot divide by zero");
+            }
+        }
+        public void clear()
+        {
+            result = 0;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            c.Add(5);
+            Console.WriteLine("Result: " + c.result);
+
+            c.sub(2);
+            Console.WriteLine("Result: " + c.result);
+
+            c.mul(3);
+            Console.WriteLine("Result: " + c.result);
+
+            c.div(2);
+            Console.WriteLine("Result: " + c.result);
+
+            c.clear();
+            Console.WriteLine("Result after clearing: " + c.result);
+            Console.ReadKey();
+        }
+    }
+}
