e40c3e6241eb2f7c86fb12f5825d6299117a2709
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-12-06 21:09:43 +0530

    c_sharp/

---
+++ c_sharp/WINDOWS_FORM/Product_Details.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Product_Details
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            comboBox2.Text = "";
+            switch(comboBox1.Text)
+            {
+                case "Tools":
+                    comboBox2.Items.Add("Makeup Brush");
+                    comboBox2.Items.Add("Air brush");
+                    comboBox2.Items.Add("Mannicure kit");
+                    comboBox2.Items.Add("Eyelash Curler");
+                    comboBox2.Items.Add("Straighting machine ");
+                    comboBox2.Items.Add("Crimmping  machine ");
+                    break;
+                case "Brands Available":
+                    comboBox2.Items.Add("Lakme");
+                    comboBox2.Items.Add("Sugar");
+                    comboBox2.Items.Add("Nyka");
+                    comboBox2.Items.Add("Color Bar");
+                    break;
+      
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            int quantity = Convert.ToInt32(textBox2.Text+"\n");
+            sb.Append("Customer Name:- " + textBox1.Text + "\n");
+            sb.Append("Customer Mobile Number:- " + maskedTextBox1.Text + "\n");
+            sb.Append("Products :- " + comboBox1.Text + "\n");
+            sb.Append("Product Name:- " + comboBox2.Text + "\n");
+            sb.Append("Quantity:- " + quantity + "\n");
+            int rate = Convert.ToInt32(textBox3.Text);
+            sb.Append("Rate:- " + rate + "\n");
+            int total = quantity * rate;
+            sb.Append("Total:- " + total + "\n");
+            label7.Text = "Display:- " + sb.ToString();
+        }
+    }
+}
---
+++ c_sharp/WINDOWS_FORM/Submit_and_Clear.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Submit_and_Clear
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            string name = textBox1.Text;
+            int age = Convert.ToInt32(numericUpDown1.Value);
+
+            if(textBox1.Text=="" && numericUpDown1.Value==0)
+            {
+                MessageBox.Show("Enter Name and Age");
+            }
+            else if(textBox1.Text=="")
+            {
+                MessageBox.Show("Enter Your Name");
+            }
+            if(numericUpDown1.Value==0)
+            {
+                MessageBox.Show("Enter age" );
+            }
+            else if (numericUpDown1.Value <=18)
+            {
+                MessageBox.Show("Your Age should be greater than 18");
+            }
+            sb.Append("Name:- " + name+"\n");
+            sb.Append("Age:- " + age);
+            label3.Text = sb.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            numericUpDown1.Value = 0;
+        }
+    }
+}
