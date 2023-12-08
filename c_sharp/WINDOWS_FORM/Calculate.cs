7c03c8f8c3460fe1bfd6b68b9ca0e0a0789defe3
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-12-08 21:31:21 +0530

    c_sharp/

---
+++ c_sharp/WINDOWS_FORM/Calculate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Add_Sub_Mul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int res = n1 + n2;
+            label3.Text = "Addition of Two Numbers: " + res;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int res = n1 - n2;
+            label3.Text = "Substraction of Two Numbers: " + res;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int res = n1 * n2;
+            label3.Text = "Multiplication of Two Numbers: " + res;
+        }
+    }
+}
