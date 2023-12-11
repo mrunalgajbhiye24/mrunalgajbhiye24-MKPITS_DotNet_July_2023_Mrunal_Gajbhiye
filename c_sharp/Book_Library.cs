f5b9ec0ab33ebe37396f40d860b16fbb21fde0f3
mrunalgajbhiye24 <mrunal2920@gmail.com>
2023-12-11 17:13:19 +0530

    c_sharp/

---
+++ c_sharp/Book_Library.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Library
+{
+    class Author
+    {
+        public string AuthorName { get; }
+
+        public Author(string authorName)
+        {
+            AuthorName = authorName;
+        }
+    }
+
+    class Book
+    {
+        public string Title { get; }
+        public string ISBN { get; }
+        public Author Author { get; }
+
+        public Book(string title, string isbn, Author author)
+        {
+            Title = title;
+            ISBN = isbn;
+            Author = author;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title}, ISBN: {ISBN}, Author: {Author.AuthorName}";
+        }
+    }
+
+    class Library
+    {
+        private List<Book> books = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+            Console.WriteLine($"Book added to the library: {book}");
+        }
+
+        public void RemoveBook(string isbn)
+        {
+            Book bookToRemove = null;
+
+            foreach (var book in books)
+            {
+                if (book.ISBN == isbn)
+                {
+                    bookToRemove = book;
+                    break;
+                }
+            }
+
+            if (bookToRemove != null)
+            {
+                books.Remove(bookToRemove);
+                Console.WriteLine($"Book removed from the library: {bookToRemove}");
+            }
+            else
+            {
+                Console.WriteLine($"Book with ISBN {isbn} not found in the library.");
+            }
+        }
+
+        public void DisplayBooks()
+        {
+            Console.WriteLine("Books in the library:");
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create authors
+            Author author1 = new Author("John Doe");
+            Author author2 = new Author("Jane Smith");
+
+            // Create books
+            Book book1 = new Book("Introduction to C#", "123456", author1);
+            Book book2 = new Book("Introduction to C#", "789012", author2);
+            Book book3 = new Book("Advanced C#", "345678", author1);
+
+            // Create library
+            Library library = new Library();
+
+            Console.WriteLine();
+            // Add books to the library
+            library.AddBook(book1);
+            library.AddBook(book2);
+            library.AddBook(book3);
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+            // Display books in the library
+            library.DisplayBooks();
+
+            Console.WriteLine("------Book Removed From the Library------");
+            // Remove a book from the library
+            library.RemoveBook("789012");
+
+            Console.WriteLine();
+            Console.WriteLine("-------Book Remains in the Library-------");
+            // Display updated books in the library
+            library.DisplayBooks();
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
---
+++ c_sharp/HierarchicalAnimalExample.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Hierarchicalexample
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Generic animal sound");
+        }
+    }
+
+    public class Mammal : Animal
+    {
+        public Mammal(string name, int age) : base(name, age)
+        {
+        }
+    }
+
+
+    public class Reptile : Animal
+    {
+        public Reptile(string name, int age) : base(name, age)
+        {
+        }
+    }
+
+    public class Bird : Animal
+    {
+        public Bird(string name, int age) : base(name, age)
+        {
+        }
+    }
+
+
+    public class Lion : Mammal
+    {
+        public Lion(string name, int age) : base(name, age)
+        {
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Lion Sounds Roar Roar!");
+        }
+    }
+
+    public class Snake : Reptile
+    {
+        public Snake(string name, int age) : base(name, age)
+        {
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Snake sound Hiss Hiss!");
+        }
+    }
+
+    public class Eagle : Bird
+    {
+        public Eagle(string name, int age) : base(name, age)
+        {
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Eagle sound Screech Screech!");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            // Create Instances of Different animal types
+            Lion lion = new Lion("Simba", 5);
+            Snake snake = new Snake("Nagini", 3);
+            Eagle eagle = new Eagle("Thunder", 2);
+
+            // Demonstrate Polymorphism by Calling the Common Method on Objects of Different Derived Classes
+            Animal[] animals = { lion, snake, eagle };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"Name: {animal.Name}, Age: {animal.Age}");
+                animal.MakeSound();
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+    }  
+    
+}
