27e6531bfd846c11e9fc670c7663f4c4af2dbe28
mrunalgajbhiye24 <mrunal2920@gmail.com>
2024-01-01 23:10:49 +0530

    Cpp/

---
+++ Cpp/perfect no for loop.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	int i;
+	int sum;
+	printf("\n enter a num");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n==i);
+		sum=sum+i;
+	{
+		if(sum==i);
+	printf("\n%d it is not a perfect num",n);
+	}
+    printf("\n%d it is a perfect num",n);
+    }
+	
+}
